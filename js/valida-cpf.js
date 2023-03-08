b51666b93a883b93e42244e7269ddfe2d53729bc
Marcos Vinícius Silva <m_vinicius94@outlook.com>
2023-03-08 07:04:14 -0300

    Validando segundo digito do cpf

--- js/valida-cpf.js
+++ js/valida-cpf.js
@@ -1,8 +1,10 @@
 export default function ehUmCpf(campo) {
     const cpf = campo.value.replace(/\.|-/g, "");
-    validaNumerosRepetidos(cpf)
-    validaPrimeiroDigito(cpf)
-    console.log(validaPrimeiroDigito(cpf));
+    if (validaNumerosRepetidos(cpf) || validaPrimeiroDigito(cpf) || validaSegundoDigito(cpf)) {
+        console.log("O CPF é invalido!")
+    } else {
+        console.log("O CPF está correto!")
+    }
 }
 
 function validaNumerosRepetidos(cpf) {
@@ -37,4 +39,22 @@ function validaPrimeiroDigito(cpf) {
     }
 
     return soma != cpf[9];
+}
+
+function validaSegundoDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 11;
+
+    for (let tamanho = 0; tamanho < 10; tamanho++) {
+        soma += cpf[tamanho] * multiplicador
+        multiplicador--
+    }
+
+    soma = (soma * 10) % 11;
+
+    if (soma == 10 || soma == 0) {
+        soma = 0;
+    }
+
+    return soma != cpf[10];
 }
